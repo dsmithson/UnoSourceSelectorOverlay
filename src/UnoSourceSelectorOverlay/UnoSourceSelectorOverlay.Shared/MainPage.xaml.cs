3387566442075d7d9988810dbda1b098b7998768
Derek Smithson <derek.smithson@gpsinsight.com>
2021-11-21 16:45:52 -0700

    Adding UWP, WASM, and Gtk projects

---
+++ src/UnoSourceSelectorOverlay/UnoSourceSelectorOverlay.Shared/MainPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnoSourceSelectorOverlay.ViewModels;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.System;
+using Windows.UI.Core;
+using System.Threading.Tasks;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UnoSourceSelectorOverlay
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            CoreWindow.GetForCurrentThread().Dispatcher.AcceleratorKeyActivated += Dispatcher_AcceleratorKeyActivated;
+        }
+
+        private VirtualKey lastKey;
+        private async void Dispatcher_AcceleratorKeyActivated(CoreDispatcher sender, AcceleratorKeyEventArgs e)
+        {
+            if (e.EventType != CoreAcceleratorKeyEventType.KeyDown)
+                return;
+
+            if (e.VirtualKey == VirtualKey.Scroll)
+            {
+                if (lastKey == VirtualKey.Scroll)
+                {
+                    //Double scroll lock
+                    viewModel.ToggleMenuVisibility();
+
+                    if (sourceList.Visibility == Visibility.Visible)
+                    {
+                        await Task.Delay(50);
+                        sourceList.Focus(FocusState.Programmatic);
+                    }
+
+                    //Force different last key so two new ScrLk presses are requried
+                    lastKey = VirtualKey.A;
+                    return;
+                }
+            }
+            else if (e.VirtualKey == VirtualKey.Escape)
+            {
+                //Hide the menu on return or enter
+                viewModel.MenuVisibility = Visibility.Collapsed;
+            }
+            else if (e.VirtualKey == VirtualKey.Enter)
+            {
+                //Select the currently selected source in our listView
+                if(sourceList.SelectedItems.Count > 0)
+                    await viewModel.SelectActiveSource(viewModel.SelectedSource);
+            }
+
+            lastKey = e.VirtualKey;
+        }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            await viewModel.RefreshSourceList();
+        }
+    }
+}
---
+++ src/UnoSourceSelectorOverlay/UnoSourceSelectorOverlay.Shared/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+using Windows.UI;
+
+namespace UnoSourceSelectorOverlay.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private Visibility menuVisibility = Visibility.Visible;
+        public Visibility MenuVisibility
+        {
+            get { return menuVisibility; }
+            set
+            {
+                if(menuVisibility != value)
+                {
+                    menuVisibility = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Source currently selected in source selection UI
+        /// </summary>
+        public SourceViewModel SelectedSource
+        {
+            get { return selectedSource; }
+            set
+            {
+                if(selectedSource != value)
+                {
+                    selectedSource = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private SourceViewModel selectedSource;
+
+        /// <summary>
+        /// Source actively being displayed on the output
+        /// </summary>
+        public SourceViewModel ActiveSource
+        {
+            get { return activeSource; }
+            set
+            {
+                if (activeSource != value)
+                {
+                    activeSource = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private SourceViewModel activeSource;
+
+        private List<SourceViewModel> sourceList;
+        public List<SourceViewModel> SourceList
+        {
+            get { return sourceList; }
+            set
+            {
+                if(sourceList != value)
+                {
+                    sourceList = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private bool sourceListLoading;
+        public bool SourceListLoading
+        {
+            get
+            {
+                return sourceListLoading;
+            }
+            set
+            {
+                if(sourceListLoading != value)
+                {
+                    sourceListLoading = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public async Task SelectActiveSource(SourceViewModel source)
+        {
+            if (source == null)
+                return;
+
+            //Simulate some network call to set the source
+            await Task.Delay(100);
+
+            //Update our selected source
+            this.ActiveSource = source;
+        }
+
+        public async Task RefreshSourceList()
+        {
+            if (SourceListLoading)
+                return;
+
+            SourceListLoading = true;
+
+            //Simulate some network call to get a source list
+            await Task.Delay(500);
+
+            var newSourceList = new List<SourceViewModel>()
+            {
+                new SourceViewModel() { Name = "Source 1", Thumbnail = new SolidColorBrush(Colors.Black) },
+                new SourceViewModel() { Name = "Source 2", Thumbnail = new SolidColorBrush(Colors.Purple) },
+                new SourceViewModel() { Name = "Source 3", Thumbnail = new SolidColorBrush(Colors.Blue) },
+                new SourceViewModel() { Name = "Source 4", Thumbnail = new SolidColorBrush(Colors.Brown) },
+                new SourceViewModel() { Name = "Source 5", Thumbnail = new SolidColorBrush(Colors.Chartreuse) },
+                new SourceViewModel() { Name = "Source 6", Thumbnail = new SolidColorBrush(Colors.Goldenrod) }
+            };
+
+            //Set the new Source List
+            SourceList = newSourceList;
+
+            //Set a default selected source
+            if(newSourceList == null || !newSourceList.Contains(this.SelectedSource))
+            {
+                this.SelectedSource = newSourceList?.FirstOrDefault();
+            }
+            SourceListLoading = false;
+
+        }
+
+        public void ToggleMenuVisibility()
+        {
+            if (MenuVisibility == Visibility.Visible)
+                MenuVisibility = Visibility.Collapsed;
+            else
+                MenuVisibility = Visibility.Visible;
+        }
+    }
+}
